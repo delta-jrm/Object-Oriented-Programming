8a76b3d357d10187d74d2a0b645de5110b159024
deltajrm <jocelynrmurray@lewisu.edu>
2019-09-18 17:15:44 -0500

    Fixed FaceDraw, FaceDrawWorking is not working

--- FaceDraw/FaceDrawWorking.java
+++ FaceDraw/FaceDrawWorking.java
@@ -7,28 +7,50 @@ import java.util.*;
 
  class Face extends JPanel
 {
-    
-    Random generator = new Random();
-    int facetype = generator.nextInt(3);
-    int facex = generator.nextInt(200);
-    int facey = generator.nextInt(200);
-    int facewidth = generator.nextInt(700) + 25;
-    int faceheight = facewidth;
-    int eyeline = facey + (faceheight/4);
-    int eyex1 = facex + (facewidth/3);
-    int eyex2 = facex + facewidth - (facewidth/3);
-    int mouthx1 = facex + (facewidth/4);
-    int mouthy1 = eyeline + (faceheight/3);
-    int mouthx2 = facex + facewidth - (facewidth/4);
-    int mouthy2 = eyeline + (faceheight/3);    
-    int mouthheight = faceheight/3;
-    int mouthwidth = facewidth/2;
+    private int facetype;
+    private int facex;
+    private int facey;
+    private int facewidth;
+    private int faceheight;
+    private int eyeline;
+    private int eyex1;
+    private int eyex2;
+    private int mouthx1;
+    private int mouthy1;
+    private int mouthheight;
+    private int mouthwidth;
 
-    Face(){
+    // Setter for type Face.
+    public void setType(int facetype) {
+        facetype = facetype;
     }
 
-    protected void paintComponent(Graphics g) {
+    // Getter for type Face.
+    public int getType() {
+        return facetype;
+    }
+
+    Face(){
+        facetype = 1;
+    }
+
+    Face(int facetype){
+        setType(facetype);
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        facex = (((int)Math.random()) * (( 1000 - 50) + 1)) + 50;
+        facey = (((int)Math.random()) * (( 1000 - 50) + 1)) + 50; 
+        facewidth = (((int)Math.random()) * (( 700 - 100) + 1)) + 100;
+        faceheight = (((int)Math.random()) * (( 700 - 100) + 1)) + 100;
+        eyeline = facey + 30;
+        eyex1 = facex + 30; 
+        eyex2 = facex + 70;
+        mouthx1 = facex + 30;
+        mouthy1 = facey + 50;
+        mouthwidth = facewidth - 50;
+        mouthheight = faceheight - 50;
         
         g.setColor( Color.black);
         g.drawArc(facex, facey, facewidth, faceheight, 0, 360);
@@ -38,61 +60,60 @@ import java.util.*;
         g.drawArc( eyex2, eyeline, 10, 10, 0, 360);
 
         //g.setColor( Color.black);
-        //g.drawArc( 100, 100, mouthwidth, mouthheight, 0, 180);
+        //g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, 180);
 
-        if (facetype == 2) {
+        if (facetype == 1) {
         g.setColor( Color.black);
         g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, -180 );
         }
 
-        if (facetype == 1) {
+        if (facetype == 0) {
         g.setColor( Color.black);
         g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, 180);
         
         }
 
-        if (facetype == 0) {
+        if (facetype == 2) {
         g.setColor( Color.black);
-        g.drawLine( mouthx1, mouthy1, mouthx2, mouthy2);
+        g.drawLine( mouthx1, mouthy1, mouthwidth, mouthheight);
 
         }
         }
         //System.out.println("this means that the drawface method works");
-    }
 
+}
 
 
 class FaceFrame extends JFrame {
 
     FaceFrame() {
 
-        // The below segment sets up the frame, titled "OvalDraw", dimensions, and visibility.
-        setTitle("FaceDraw");
-        setSize(1000,1000); 
+        
+        setTitle("FaceFrame");
+        setBounds(200,200,300,400); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Builds new Oval Object in color Green (Neon-ish Green/Yellow).
+
         Face face1 = new Face();
-        Face face2 = new Face();
 
         // Builds a Content Pane to display the Oval within.
         Container contentPane = getContentPane(); 
 
         // Adds the Oval to the Content Pane.
-        contentPane.add(face1);
-        contentPane.add(face2);
+        contentPane.add(face1); 
+             
     }
 }
 
 public class FaceDraw {
     public static void main(String[] args) {
         System.out.println("FaceDraw Starting...");
-        //Builds new object OvalFrame to display the Oval.
-        //for(int i=0; i <= 10; i++);
-        FaceFrame myFrame = new FaceFrame();
-        myFrame.setVisible(true);
-        //}
+
         
 
+        //Builds new object OvalFrame to display the Oval.
+        FaceFrame myFrame = new FaceFrame();
+        myFrame.setVisible(true);
     }
 }
