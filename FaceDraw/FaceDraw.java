01f4a275dff42852c68f7e00c61c663299dbbd44
deltajrm <jocelynrmurray@lewisu.edu>
2019-09-20 12:33:13 -0500

    finished facedraw for the first time

--- FaceDraw/FaceDraw.java
+++ FaceDraw/FaceDraw.java
@@ -5,31 +5,69 @@ import javax.swing.*;
 import java.lang.Math;
 import java.util.*;
 
- class Face extends JPanel
-{
-    
-    Random generator = new Random();
-    int facetype = generator.nextInt(3);
-    int facex = generator.nextInt(200);
-    int facey = generator.nextInt(200);
-    int facewidth = generator.nextInt(700) + 25;
-    int faceheight = facewidth;
-    int eyeline = facey + (faceheight/4);
-    int eyex1 = facex + (facewidth/3);
-    int eyex2 = facex + facewidth - (facewidth/3);
-    int mouthx1 = facex + (facewidth/4);
-    int mouthy1 = eyeline + (faceheight/3);
-    int mouthx2 = facex + facewidth - (facewidth/4);
-    int mouthy2 = eyeline + (faceheight/3);    
-    int mouthheight = faceheight/3;
-    int mouthwidth = facewidth/2;
-
-    Face(){
+class FaceFrame extends JPanel {
+    ArrayList<Face> faces;
+    FaceFrame() {
+        faces = new ArrayList<>();
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
+        faces.add(new Face());
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+        for(Face face : faces) {
+            face.drawFace(g);
+        }
+    }
+}
+
+class Face extends JPanel
+{
+    private ArrayList<Face> faces = new ArrayList<>();
+    Random generator = new Random();
+    int facetype;
+    int facex;
+    int facey;
+    int facewidth;
+    int faceheight;
+    int eyeline;
+    int eyex1;
+    int eyex2;
+    int mouthx1;
+    int mouthy1;
+    int mouthx2;
+    int mouthy2;    
+    int mouthheight;
+    int mouthwidth;
+
+    public Face(){
+        Random generator = new Random();
+        this.facetype = generator.nextInt(3);
+        this.facex = generator.nextInt(200);
+        this.facey = generator.nextInt(200);
+        this.facewidth = generator.nextInt(700) + 25;
+        this.faceheight = facewidth;
+        this.eyeline = facey + (faceheight/4);
+        this.eyex1 = facex + (facewidth/3);
+        this.eyex2 = facex + facewidth - (facewidth/3);
+        this.mouthx1 = facex + (facewidth/4);
+        this.mouthy1 = eyeline + (faceheight/3);
+        this.mouthx2 = facex + facewidth - (facewidth/4);
+        this.mouthy2 = eyeline + (faceheight/3);    
+        this.mouthheight = faceheight/3;
+        this.mouthwidth = facewidth/2;
+
+    } 
+    
+    public void drawFace(Graphics g) {
         g.setColor( Color.black);
         g.drawArc(facex, facey, facewidth, faceheight, 0, 360);
 
@@ -41,91 +79,39 @@ import java.util.*;
         //g.drawArc( 100, 100, mouthwidth, mouthheight, 0, 180);
 
         if (facetype == 2) {
-        g.setColor( Color.black);
-        g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, -180 );
+            g.setColor( Color.black);
+            g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, -180 );
         }
 
         if (facetype == 1) {
-        g.setColor( Color.black);
-        g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, 180);
+            g.setColor( Color.black);
+            g.drawArc( mouthx1, mouthy1, mouthwidth, mouthheight, 0, 180);
         
         }
 
         if (facetype == 0) {
-        g.setColor( Color.black);
-        g.drawLine( mouthx1, mouthy1, mouthx2, mouthy2);
+            g.setColor( Color.black);
+            g.drawLine( mouthx1, mouthy1, mouthx2, mouthy2);
 
         }
-        }
-        //System.out.println("this means that the drawface method works");
     }
 
-
-
-class FaceFrame extends JFrame {
-
-    FaceFrame() {
-        // The below segment sets up the frame, titled "OvalDraw", dimensions, and visibility.
-        setTitle("FaceDraw");
-        setSize(1000,1000); 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Builds new Oval Object in color Green (Neon-ish Green/Yellow).
-        Face face1 = new Face();
-        Face face2 = new Face();
-        Face face3 = new Face();
-        Face face4 = new Face();
-        Face face5 = new Face();
-        Face face6 = new Face();
-        Face face7 = new Face();
-        Face face8 = new Face();
-        Face face9 = new Face();
-        Face face10 = new Face();        
-        // Builds a Content Pane to display the Oval within.
-        Container contentPane = getContentPane(); 
-
-        // Adds the Oval to the Content Pane.
-        getContentPane().add(face1);
-        revalidate();
-        repaint();
-        getContentPane().add(face2);
-        revalidate();
-        repaint();
-        getContentPane().add(face3);
-        revalidate();
-        repaint();
-        getContentPane().add(face4);
-        revalidate();
-        repaint();
-        getContentPane().add(face5);
-        revalidate();
-        repaint();
-        getContentPane().add(face6);
-        revalidate();
-        repaint();
-        getContentPane().add(face7);
-        revalidate();
-        repaint();
-        getContentPane().add(face8);
-        revalidate();
-        repaint();
-        getContentPane().add(face9);
-        revalidate();
-        repaint();
-        getContentPane().add(face10);
-        revalidate();
-        repaint();
-
+   
     }
-}
 
-public class FaceDraw {
+public class FaceDraw extends JFrame {
+
     public static void main(String[] args) {
         System.out.println("FaceDraw Starting...");
         //Builds new object OvalFrame to display the Oval.
         //for(int i=0; i <= 10; i++);
-        FaceFrame myFrame = new FaceFrame();
-        myFrame.setVisible(true);
+        JFrame frame = new JFrame();
+        frame.setTitle("FaceDraw");
+        frame.setSize(1000,1000); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new FaceFrame());
+        frame.setVisible(true);
+
         //}
 
       }
